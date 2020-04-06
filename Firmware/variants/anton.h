2fc460f13ff1847ce1249f8296c5c8f1f5652759
Thomas Scheiblauer <tom@sharkbay.at>
2020-04-06 12:59:40 +0200

    add personalized variants

---
+++ Firmware/variants/anton.h
@@ -0,0 +1,13 @@
+#ifndef VARIANT_ANTON_H
+#define VARIANT_ANTON_H
+
+/*------------------------------------
+ MODDING SETTINGS
+ *------------------------------------*/
+//#define BONDTECH_EXTRUDER
+#define STEPPER_X_09
+#define STEPPER_Y_09
+
+#include "1_75mm_MK3S-EINSy10a-E3Dv6full.h"
+
+#endif //__VARIANT_ANTON_H
---
+++ Firmware/variants/tom.h
@@ -0,0 +1,13 @@
+#ifndef VARIANT_TOM_H
+#define VARIANT_TOM_H
+
+/*------------------------------------
+ MODDING SETTINGS
+ *------------------------------------*/
+#define BONDTECH_EXTRUDER
+#define STEPPER_X_09
+#define STEPPER_Y_09
+
+#include "1_75mm_MK3S-EINSy10a-E3Dv6full.h"
+
+#endif //__VARIANT_TOM_H
